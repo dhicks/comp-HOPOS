6f17eaece58c1e3ba1cf31ba921eda0ff945ae67
Dan Hicks <hicks.daniel.j@gmail.com>
2018-08-04 09:55:19 -0700

    add & reorg journal lists

--- 01_journals.R
+++ 01_journals.R
@@ -3,45 +3,61 @@ library(tidyverse)
 library(lubridate)
 library(rcrossref)
 
-## Bc some philosophy of science is published elsewhere (especially before the phil sci/analytic split began), need to grab both "primary" phil sci journals + "secondary" journals that have a little phil sci (and lots of irrelevant stuff)
-## To filter out the irrelevant stuff later, use inclusion rules for authors after de-duping, eg, need at least 2 papers in primary journals
+## Bc some philosophy of science is published elsewhere (especially before the phil sci/analytic split began), need to grab both "primary" phil sci journals + "secondary" journals that have a little phil sci (and lots of other stuff)
+## To filter out the other stuff later, use inclusion rules for authors after de-duping, eg, need at least 2 papers in primary journals
 
 issns_primary = c('0031-8248', # Philosophy of Science
-                  '0039-3681', # Studies A
-                  '1355-2198', # Studies B
-                  '1369-8486', # Studies C
+                  '0270-8647', # PSA Proceedings
                   '0169-3867', # Biology and Philosophy
                   '0007-0882', # BJPS
                   '1464-3537', ## Need both ISSNs to get all BJPS results
                   '2152-5188', # HOPOS
                   '1879-4912', # EJPS
                   '1063-6145', # Perspectives on Science
-                  '1949-0739' # PTP Bio
+                  '1949-0739', # PTP Bio
+                  '0269-8595', # International Studies in the Philosophy of Science
+                  '0925-4560', # Journal of General Philosophy of Science
+                  '0048-3931' # Philosophy of the Social Sciences
 )
 issns_secondary = c(
+    ## ----- Boundary btwn phil sci and other disciplines
+    '0039-3681', # Studies A
+    '1355-2198', # Studies B
+    '1369-8486', # Studies C
+    '1233-1821', # Foundations of Science
+    '1350-178X', # Journal of Economic Methodology
+    '0015-9018', # Foundations of Physics
+    '2069-0533', # Logos and Episteme
+    '1913-0465', # Spontaneous Generations
+    '1474-0664', # Science in Context
+    '0926-7220', # Science and Education
+    '0162-2439' # ST&HV
+)
+issns_feminist = c(
+    ## ----- Feminist phil
+    '0887-5367', # Hypatia
+    '0097-9740' # Signs
+)
+issns_analytic = c(
     ## ----- Boundary btwn phil sci and analytic
     '0039-7857', # Synthese
     '0165-0106', # Erkenntnis
     '0269-1728', # Social Epistemology
-    ## ----- Less prominent/more specialized phil sci
-    '0269-8595', # International Studies in the Philosophy of Science
-    '1233-1821', # Foundations of Science
-    '0815-0796', # Metascience
-    '0048-3931', # Philosophy of the Social Sciences
-    '1350-178X', # Journal of Economic Methodology
-    '0015-9018', # Foundations of Physics
-    '0925-4560', # Journal of General Philosophy of Science
-    '2069-0533', # Logos and Episteme
-    ## ----- Feminist phil
-    '0887-5367', # Hypatia
-    '0097-9740', # Signs
     ## ----- Analytic
     '0003-2638', # Analysis
     '0022-362X', # J Phil
     '0031-8116', # Phil Studies
     '0031-8205', # PPR
     '2330-4014', # Ergo
-    '0026-9662' # Monist
+    '0026-9662', # Monist
+    '0026-4423', # Mind
+    '1467-9264', # Proc. Aristotelean Soc.
+    '0031-8108', # Phil Review
+    #'0003-0481' # APQ - not indexed by CrossRef
+    '0029-4624', # Nous
+    '1572-8749', # Topoi
+    '1742-3600', # Episteme
+    '1467-9973' # Metaphilosophy
 )
 
 ## NB Hypatia has articles with multiple DOIs, eg, 10.1111/j.1527-2001.2000.tb01079.x, 10.1353/hyp.2000.0002, and 10.2979/hyp.2000.15.1.43
@@ -50,9 +66,12 @@ issns_secondary = c(
 ## It looks like these are book reviews:  Wylie groups the book reviews in an issue together under a single DOI
 ## TRY:  only use the 1. Hypatia ISSN, then filter out the "hyp" DOIs
 
-issns = tibble(issn = c(issns_primary, issns_secondary), 
+issns = tibble(issn = c(issns_primary, issns_secondary, 
+                        issns_feminist, issns_analytic), 
                publication_group = c(rep('primary', length(issns_primary)), 
-                                     rep('secondary', length(issns_secondary))))
+                                     rep('secondary', length(issns_secondary)), 
+                                     rep('feminist', length(issns_feminist)), 
+                                     rep('analytic', length(issns_analytic))))
 
 journal_data = issns %>%
     pull(issn) %>%
@@ -75,6 +94,7 @@ system.time({
 
 papers = results$data %>%
     filter(type != 'journal', type != 'journal-issue', 
+           ## Deal w/ the Hypatia duplication issue noted above
            !str_detect(doi, 'hyp')) %>%
     separate(issn, c('issn', 'issn2'), sep = ',', fill = 'right') %>% 
     select(-issn2) %>%
